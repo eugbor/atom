1e03e4995346e04d6f381398cd8609f4d33cb7b7
eugbor <eugeneborodin@mail.ru>
2019-01-25 11:07:20 +0300

    HW1-2

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -6,20 +6,25 @@ package ru.atom;
  *  throw new UnsupportedOperationException(); - is just a stub
  */
 public class Util {
+
     /**
      * Returns the greatest of {@code int} values.
      *
      * @param values an argument. Assume values.length > 0.
      * @return the largest of values.
      */
+
     public static int max(int[] values) {
+
         int aaa = 0;
+
         for (int i = 0; i < values.length; i++) {
 
             if (values[i] > aaa){
                 aaa = values[i];
             }
         }
+
         return aaa;
     }
 
@@ -30,12 +35,14 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
+
         long aaa = 0;
+
         for (int i = 0; i < values.length; i++) {
             aaa = aaa + values[i];
         }
+
         return aaa;
     }
-
 
 }
