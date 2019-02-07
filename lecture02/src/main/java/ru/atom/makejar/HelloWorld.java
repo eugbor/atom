60191b6f36e70eeaa07057ed26900442eed47df7
eugbor <eugeneborodin@mail.ru>
2019-02-07 16:55:41 +0300

    HW2-6

--- lecture02/src/main/java/ru/atom/makejar/HelloWorld.java
+++ lecture02/src/main/java/ru/atom/makejar/HelloWorld.java
@@ -1,16 +1,10 @@
 package ru.atom.makejar;
 
-import ru.atom.geometry.Collider;
-import ru.atom.geometry.Point;
-
 /**
  * Class just to test fat jar packing
  */
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println(getHelloWorld());
-        }
-
+    public static void main(String[] args) { System.out.println(getHelloWorld()); }
     public static String getHelloWorld() {
         return "Hello, World!";
     }
